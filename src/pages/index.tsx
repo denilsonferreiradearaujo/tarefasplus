c04fd0c0c5dd16210cfa5ce765c54858e34ffc80
Denilson Ferreira de Araujo <denilson.araujo.programador@gmail.com>
2025-04-04 22:55:13 -0300

    Atualizando local do home.module.css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/home.module.css";
+import styles from "../../styles/home.module.css";
 
 import heroImg from "../../public/assets/hero.png"
 import { GetStaticProps } from "next";
